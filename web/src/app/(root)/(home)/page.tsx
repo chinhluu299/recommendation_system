996aaa602cd24ec1caa37445c9b8b4685d49d5f6
mielindev <phamvietquang.dev@gmail.com>
2026-03-10 13:46:02 +0700

    feat: enhance Home layout with Banner component and improve layout responsiveness

--- web/src/app/(root)/(home)/page.tsx
+++ web/src/app/(root)/(home)/page.tsx
@@ -1,6 +1,10 @@
+import Banner from "@/components/features/Banner";
+
 export default function Home() {
   return (
     <main className="min-h-screen">
+      <Banner />
+
       {/* Đây là nơi bạn sẽ đặt Hero Section và Product Grid của Shopcart */}
       <section className="container mx-auto py-10">
         <h1 className="text-4xl font-bold">Headphones For You!</h1>
--- web/src/app/(root)/layout.tsx
+++ web/src/app/(root)/layout.tsx
@@ -3,7 +3,7 @@ import Header from "@/components/shared/Header";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="min-h-screen flex flex-col px-16 mx-auto">
+    <div className="min-h-screen flex flex-col px-3 sm:px-4 md:px-6 lg:px-16 mx-auto">
       <Header />
       {children}
       <Footer />
