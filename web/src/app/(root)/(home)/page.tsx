e12ef34549bb893c4d8954c065c7bc98384c954b
mielindev <phamvietquang.dev@gmail.com>
2026-03-11 15:20:17 +0700

    feat: enhance Home page layout with product section and navigation button

--- web/src/app/(root)/(home)/page.tsx
+++ web/src/app/(root)/(home)/page.tsx
@@ -1,17 +1,36 @@
 import Banner from "@/components/features/Banner";
+import ProductList from "@/components/features/ProducList";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <main className="min-h-screen">
       <Banner />
 
-      {/* Đây là nơi bạn sẽ đặt Hero Section và Product Grid của Shopcart */}
-      <section className="container mx-auto py-10">
-        <h1 className="text-4xl font-bold">Headphones For You!</h1>
-        <p className="text-muted-foreground mt-2">
-          Explore our latest recommendations based on your style.
-        </p>
+      <section className="mx-auto w-full max-w-7xl px-3 pt-4 sm:px-4 sm:pt-6 md:px-0 md:pt-8 lg:pt-10">
+        <div className="mb-2 flex flex-col items-start gap-4 sm:gap-2 md:flex-row md:items-center md:justify-between">
+          <div>
+            <h2 className="mb-2 text-2xl font-bold text-gray-900 sm:text-3xl lg:text-4xl">
+              Our Products
+            </h2>
+            <p className="text-sm text-gray-600 sm:text-base">
+              Discover our carefully curated selection of premium mobile
+              products.
+            </p>
+          </div>
+          <Button
+            asChild
+            variant="outline"
+            className="mt-4 rounded-full border-emerald-600 text-emerald-700 hover:bg-emerald-600 hover:text-white"
+          >
+            <Link href="/products">View all products</Link>
+          </Button>
+        </div>
       </section>
+
+      {/* TODO: Replace default data by calling featured products API and pass via `products` prop. */}
+      <ProductList />
     </main>
   );
 }
--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import QueryProvider from "@/components/shared/QueryProvider";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -25,9 +26,10 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased `}
+        suppressHydrationWarning
       >
-        {children}
+        <QueryProvider>{children}</QueryProvider>
       </body>
     </html>
   );
